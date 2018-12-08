bf77e79d0bd18027dad4d9ab159f0933b3614076
KMDzik <katarzyna.dzik90@gmail.com>
2018-12-08 16:08:41 +0100

    DOM variables

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'],
     values = ['Ace', 'King', 'Queen', 'Jack', 
               'Ten', 'Nine', 'Eight', 'Seven', 'Six', 
               'Five', 'Four','Three', 'Two'];
+
+//DOM variables
+let textArea = document.getElementById('text-area');
+    nameGameButton = document.getElementById('new-game-button'),
+    hitButton = document.getElementById('stay-button');
+
