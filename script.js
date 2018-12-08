4cb3f556ac8cc69ddb890a273c6d17fa398d6880
KMDzik <katarzyna.dzik90@gmail.com>
2018-12-08 16:04:19 +0100

    card variables

---
+++ script.js
@@ -0,0 +1,6 @@
+//Card variables
+
+let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'],
+    values = ['Ace', 'King', 'Queen', 'Jack', 
+              'Ten', 'Nine', 'Eight', 'Seven', 'Six', 
+              'Five', 'Four','Three', 'Two'];
