92713fd7d655a4bc2ed099d93953fac308c0fbac
KMDzik <katarzyna.dzik90@gmail.com>
2018-12-09 15:23:17 +0100

    wip

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
 //Card variables
 
-let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'],
-    values = ['Ace', 'King', 'Queen', 'Jack', 
-              'Ten', 'Nine', 'Eight', 'Seven', 'Six', 
-              'Five', 'Four','Three', 'Two'];
+let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
+let values = ['Ace', 'King', 'Queen', 'Jack',
+             'Ten', 'Nine', 'Eight', 'Seven', 'Six',
+              'Five', 'Four', 'Three', 'Two'];
 
 //DOM variables
 let textArea = document.getElementById('text-area');
-    nameGameButton = document.getElementById('new-game-button'),
-    hitButton = document.getElementById('stay-button');
+let nameGameButton = document.getElementById('new-game-button');
+let hitButton = document.getElementById('hit-button');
+let staytButton = document.getElementById('stay-button');
 
 //Game variables
 let gameStarted = false,
-    gameOver = false,
-    playerWon = false,
-    dealerCards = [],
-    playerCards = [],
-    dealerScore = 0,
-    playerScore = 0,
-    deck = [];
+  gameOver = false,
+  playerWon = false,
+  dealerCards = [],
+  playerCards = [],
+  dealerScore = 0,
+  playerScore = 0,
+  deck = [];
 
 hitButton.style.display = 'none';
 stayButton.style.display = 'none';
 showStatus();
 
 
-newGameButton.addEventListener('click', function() {
+newGameButton.addEventListener('click', function () {
   gameStarted = true;
   gameOver = false;
   playerWon = false;
 
   deck = createDeck();
-  dealerCards = [ getNextCard(), getNextCard()];
-  playerCards = [ getNextCard(), getNextCard()];
+  shuffleDeck(deck);
+  dealerCards = [getNextCard(), getNextCard()];
+  playerCards = [getNextCard(), getNextCard()];
 
   newGameButton.style.display = 'none';
   hitButton.style.display = 'inline';
@@ -41,26 +43,44 @@ newGameButton.addEventListener('click', function() {
 });
 
 function createDeck() {
-    let deck = [];
-    for(let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
-        for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
-          let card = {
-            suit: suits[suitIdx],
-            value: values[valueIdx]
-          };
-          deck.push(card);
-        }
+  let deck = [];
+  for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
+    for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
+      let card = {
+        suit: suits[suitIdx],
+        value: values[valueIdx]
+      };
+      deck.push(card);
     }
-    return deck;
+  }
+  return deck;
 }
 
+function shuffleDeck(deck) {
+  for (let i = 0; i < deck.length; i++) {
+    let swapIdx = Math.trunc(Math.random() * deck.length);
+    let tmp = deck[swapIdx];
+    deck[swapIdx] = deck[i];
+    deck[i] = tmp;
+  }
+}
+
+
 function getCardString(card) {
-  retrn card.value + 'of' + card.suit;
+  return card.value + ' of ' + card.suit;
+}
+
+function getNextCard() {
+  return deck.shift();
 }
 
 function showStatus() {
-  if(!gameStarted) {
+  if (!gameStarted) {
     textArea.innerText = "Welcome to Blackjack!";
     return;
+  }
+
+  for (var i = 0; i < deck.length; i++) {
+    textArea.innerText += '\n' + getCardString(deck[i]);
   }
 }
